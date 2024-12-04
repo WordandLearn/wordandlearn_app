fa0dc8f3afb33b78ad668cd5ff5bd5f8dd273b76
nguthiru <nguthiruedwin@gmail.com>
2024-12-04 16:35:01 +0300

    changed app icon and splash screen

--- lib/models/writing/http_response.dart
+++ lib/models/writing/http_response.dart
@@ -31,4 +31,39 @@ class HttpResponse<T> {
       'models': models,
     }.toString();
   }
+
+  String? get error {
+    return data is Map && data.containsKey('error') ? data['error'] : null;
+  }
+
+  List<String>? get errors {
+    if (isSuccess) {
+      return null;
+    }
+    if (data is Map) {
+      if (data.containsKey('errors') && data['errors'] is Map) {
+        var errors = (data['errors'] as Map).entries.map((entry) {
+          if (entry.value is List) {
+            try {
+              return '${entry.key}: ${(entry.value as List).map((e) => e.toString()).join('\n ')}';
+            } catch (e) {
+              return '${entry.key}: [Invalid Error Format]';
+            }
+          } else {
+            return '${entry.key}: ${entry.value.toString()}';
+          }
+        }).toList();
+        return errors.cast<String>();
+      }
+      if (data.containsKey('error')) {
+        return [data['error'].toString()];
+      }
+      return null;
+    }
+    return null;
+  }
+
+  String? get responseMessage {
+    return data is Map && data.containsKey('message') ? data['message'] : null;
+  }
 }
